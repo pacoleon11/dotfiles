e4d9d48ce4464bbe1b0f704c2742e2e82f6bbfb9
Paco Leon <franciscoleon92@gmail.com>
2026-01-16 07:52:12 -0800

    nvim/keymaps: <leader>q -> cclose

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,15 @@
 
 local map = vim.keymap.set
 
-map('n', '<leader>i', ":Inspect<cr>", { silent = true })
+
+map('n', '<leader>q', '<cmd>cclose<cr>', { silent = true })
+
+map('n', '<leader>i', '<cmd>Inspect<cr>', { silent = true })
 
 -- Copy
-map('n', '<leader>yw', ':let @+ = expand("<cword>")<cr>', { silent = true })
-map('n', '<leader>yW', ':let @+ = expand("<CWORD>")<cr>', { silent = true })
-map('n', '<leader>yf', ':let @+ = expand("%")<cr>', { silent = true })
-map('n', '<leader>yg', ':let @+ = expand("%:p")<cr>', { silent = true })
-map('n', '<leader>yh', ':let @+ = expand("%:t")<cr>', { silent = true })
-map('n', '<leader>yl', ":let @+ = trim(getline('.'))<cr>", { silent = true })
+map('n', '<leader>yw', '<cmd>let @+ = expand("<cword>")<cr>', { silent = true })
+map('n', '<leader>yW', '<cmd>let @+ = expand("<CWORD>")<cr>', { silent = true })
+map('n', '<leader>yf', '<cmd>let @+ = expand("%")<cr>', { silent = true })
+map('n', '<leader>yg', '<cmd>let @+ = expand("%:p")<cr>', { silent = true })
+map('n', '<leader>yh', '<cmd>let @+ = expand("%:t")<cr>', { silent = true })
+map('n', '<leader>yl', "<cmd>let @+ = trim(getline('.'))<cr>", { silent = true })
